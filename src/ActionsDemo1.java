0d0e2ca58627e0a63a55488fec825229ddb8044d
gerheart <cadalig20@gmail.com>
2022-12-02 15:56:25 +0800

    Commit from Asian Timezone Guy

--- src/ActionsDemo1.java
+++ src/ActionsDemo1.java
@@ -13,22 +13,7 @@ public class ActionsDemo1 {
 	public static void main(String[] args) {
 
 
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Documents\\chromedriver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-
-		WebDriverWait eWait = new WebDriverWait(driver, Duration.ofSeconds(5));
-		
-		driver.manage().window().maximize();
-
-		driver.get("https://www.amazon.com/");
-		
-		Actions act = new Actions(driver);
-		
-		//Hover Mouse to Search, Click, ShiftDown(capital), Type hello, ShiftUp, Type kitty, Enter
-		act.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.LEFT_SHIFT).sendKeys("hello").keyUp(Keys.LEFT_SHIFT).sendKeys(" kitty").sendKeys(Keys.ENTER).build().perform();
-
-		//Hover mouse to WebElement
-		act.moveToElement(driver.findElement(By.id("nav-link-accountList-nav-line-1"))).build().perform();
+		System.out.println("Code Updated");
 		
 	}
 
